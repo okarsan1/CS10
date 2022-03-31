5d8a38279bcf736612b2523b5704095c15d2f613
17058003 <17058003@L815WD220005.edu.cbe.ab.ca>
2022-03-31 14:30:36 -0600

    jjghghghghghgh;pp[[

---
+++ CS_10/src/FromMeToYou/frommetoyou.java
@@ -0,0 +1,30 @@
+/*
+
+Program: ${frommetoyou}.java          Date: ${date}
+
+
+Author: Omar Karsan 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+package FromMeToYou;
+
+public class frommetoyou {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+
+	}
+
+}
+
+
+/* Screen Dump
+ 
+
+
+ 
+ */
