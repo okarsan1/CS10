d83dbb96f1fc4421340ea39f36f6ba8bcbe576c6
17058003 <17058003@L815WD220005.edu.cbe.ab.ca>
2022-04-01 11:08:52 -0600

    hthyyjhhj

--- CS_10/src/FromMeToYou/frommetoyou.java
+++ CS_10/src/FromMeToYou/frommetoyou.java
@@ -1,6 +1,6 @@
 /*
 
-Program: ${frommetoyou}.java          Date: ${date}
+Program: ${frommetoyou}.java          Date: ${04-01-2022}
 
 
 Author: Omar Karsan 
@@ -11,29 +11,30 @@ Course: Computer Science 10
 */
 package FromMeToYou;
 
-public class frommetoyou {
+public class frommetoyou 
+{
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Omar Karsan 04/01/2022");
-	   
-	System.out.println("From:Mr.Wilson");
-	System.out.println("Address: Acer Computer, Lab 314");
-	System.out.println("Date: April 01 2022\n");
-	   
-	System.out.println("To: Omar Karsan\n");
-	   
-	   
-	System.out.println("Message: Help! Im trapped inside a computer!");
-		
+		   
+		System.out.println("From:Mr.Wilson");
+		System.out.println("Address: Acer Computer, Lab 314");
+		System.out.println("Date: April 01 2022\n");
+		   
+		System.out.println("To: Omar Karsan\n");
+		   
+		   
+		System.out.println("Message: Help! Im trapped inside a computer!");
+			
+		}
+
 	}
 
-}
+
+	/* Screen Dump
+	 
 
 
-/* Screen Dump
- 
-
-
- 
- */
+	 
+	 */
---
+++ CS_10/src/MyIntials/myintials.java
@@ -0,0 +1,44 @@
+/*
+
+Program: ${myintials.java}.java          Date: ${04-01-2022}
+
+
+Author: Omar Karsan 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+package MyIntials;
+
+public class myintials 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("( ͡❛ ͜ʖ ͡❛)");
+		   
+		System.out.println("▒▒▒▒▒▒▐███████▌\r\n"
+				+ "▒▒▒▒▒▒▐░▀░▀░▀░▌\r\n"
+				+ "▒▒▒▒▒▒▐▄▄▄▄▄▄▄▌\r\n"
+				+ "▄▀▀▀█▒▐░▀▀▄▀▀░▌▒█▀▀▀▄\r\n"
+				+ "▌▌▌▌▐▒▄▌░▄▄▄░▐▄▒▌▐▐▐▐");
+		System.out.println("Address: Acer Computer, Lab 314");
+		System.out.println("Date: April 01 2022\n");
+		   
+		System.out.println("To: Omar Karsan\n");
+		   
+		   
+		System.out.println("Message: Help! Im trapped inside a computer!");
+			
+		}
+
+	}
+
+
+	/* Screen Dump
+	 
+
+
+	 
+	 */
