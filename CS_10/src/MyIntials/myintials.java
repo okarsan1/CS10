db2d6ed602d5a8721b5b7a101e85f364babc9b94
17058003 <17058003@L815WD220005.edu.cbe.ab.ca>
2022-04-01 11:15:00 -0600

    jfojhrfjiiffr

--- CS_10/src/MyIntials/myintials.java
+++ CS_10/src/MyIntials/myintials.java
@@ -16,20 +16,41 @@ public class myintials
 
 	public static void main(String[] args) 
 	{
-		System.out.println("( ͡❛ ͜ʖ ͡❛)");
+		System.out.println("( ͡❛ ͜ʖ ͡❛)\n");
 		   
 		System.out.println("▒▒▒▒▒▒▐███████▌\r\n"
 				+ "▒▒▒▒▒▒▐░▀░▀░▀░▌\r\n"
 				+ "▒▒▒▒▒▒▐▄▄▄▄▄▄▄▌\r\n"
 				+ "▄▀▀▀█▒▐░▀▀▄▀▀░▌▒█▀▀▀▄\r\n"
 				+ "▌▌▌▌▐▒▄▌░▄▄▄░▐▄▒▌▐▐▐▐");
-		System.out.println("Address: Acer Computer, Lab 314");
-		System.out.println("Date: April 01 2022\n");
+		System.out.println("───────────────▄▄───▐█\r\n"
+				+ "───▄▄▄───▄██▄──█▀───█─▄\r\n"
+				+ "─▄██▀█▌─██▄▄──▐█▀▄─▐█▀\r\n"
+				+ "▐█▀▀▌───▄▀▌─▌─█─▌──▌─▌\r\n"
+				+ "▌▀▄─▐──▀▄─▐▄─▐▄▐▄─▐▄─▐▄\r\n"
+				+ "\n");
+		System.out.println("░▄▄▄▄░\r\n"
+				+ "▀▀▄██►\r\n"
+				+ "▀▀███►\r\n"
+				+ "░▀███►░█►\r\n"
+				+ "▒▄████▀▀\r\n"
+				+ "\n");
 		   
-		System.out.println("To: Omar Karsan\n");
+		System.out.println("──███▅▄▄▄▄▄▄▄▄▄\r\n"
+				+ "─██▐████████████\r\n"
+				+ "▐█▀████████████▌▌\r\n"
+				+ "▐─▀▀▀▐█▌▀▀███▀█─▌\r\n"
+				+ "▐▄───▄█───▄█▌▄█\r\n"
+				+ "\n");
 		   
 		   
-		System.out.println("Message: Help! Im trapped inside a computer!");
+		System.out.println("─────▄████▀█▄\r\n"
+				+ "───▄█████████████████▄\r\n"
+				+ "─▄█████.▼.▼.▼.▼.▼.▼▼▼▼\r\n"
+				+ "▄███████▄.▲.▲▲▲▲▲▲▲▲\r\n"
+				+ "████████████████████▀▀\n");
+		
+		System.out.println("( ͠❛  ͟ʖ͠❛ )\n");
 			
 		}
 
