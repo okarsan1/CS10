cd9ec36cdab81425087b370f2721031ce25e6322
17058003 <17058003@L815WD220005.edu.cbe.ab.ca>
2022-04-01 11:32:33 -0600

    grttyy

---
+++ CS_10/src/BingoCard/bingocard.java
@@ -0,0 +1,34 @@
+/*
+
+Program: ${frommetoyou}.java          Date: ${04-01-2022}
+
+
+Author: Omar Karsan 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+package BingoCard;
+
+public class bingocard 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("B\t I\t N\t G\t O\t");
+		System.out.println("1\t 2\t 3\t 4\t 5\t");
+		System.out.println("6\t 7\t free\t 8\t 9\t");
+		System.out.println("\t \t \t \t \t");
+		System.out.println("\t \t \t \t \t");
+		System.out.println("\t \t \t \t \t");
+
+	}
+
+}
+/* Screen Dump
+
+
+
+
+ */
