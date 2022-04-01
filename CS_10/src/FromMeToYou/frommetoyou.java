0f288810f8c7c51b9dc91a7c35b3c6b67b47b5e9
17058003 <17058003@L815WD220005.edu.cbe.ab.ca>
2022-04-01 10:58:03 -0600

    whoop

--- CS_10/src/FromMeToYou/frommetoyou.java
+++ CS_10/src/FromMeToYou/frommetoyou.java
@@ -15,8 +15,17 @@ public class frommetoyou {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World");
-
+		System.out.println("Omar Karsan 04/01/2022");
+	   
+	System.out.println("From:Mr.Wilson");
+	System.out.println("Address: Acer Computer, Lab 314");
+	System.out.println("Date: April 01 2022\n");
+	   
+	System.out.println("To: Omar Karsan\n");
+	   
+	   
+	System.out.println("Message: Help! Im trapped inside a computer!");
+		
 	}
 
 }
