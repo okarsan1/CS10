2e3705cd0dc9abcd799c0bd67a14672189d7f3e2
17058003 <17058003@L815WD220005.edu.cbe.ab.ca>
2022-04-04 13:42:06 -0600

    yrtytyy

--- CS_10/src/Pizzasite/pizzasite.java
+++ CS_10/src/Pizzasite/pizzasite.java
@@ -1,11 +1,14 @@
 package Pizzasite;
 
-public class pizzasite 
+public class pizzasite
 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+String txt = "1234567890";
+String ok = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			    System.out.println("Enter the diameter of the pizza in inches:: " + txt.length());
+			    System.out.println("The cost of making the pizza is:: " + ok.length());
 	}
 
 }
