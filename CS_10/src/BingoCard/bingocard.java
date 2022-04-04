0e2fd503a1c8848afe756eb18c4857d5b57d0469
17058003 <17058003@L815WD220005.edu.cbe.ab.ca>
2022-04-04 12:51:39 -0600

    tyytygrtytrtr

--- CS_10/src/BingoCard/bingocard.java
+++ CS_10/src/BingoCard/bingocard.java
@@ -19,9 +19,9 @@ public class bingocard
 		System.out.println("B\t I\t N\t G\t O\t");
 		System.out.println("1\t 2\t 3\t 4\t 5\t");
 		System.out.println("6\t 7\t free\t 8\t 9\t");
-		System.out.println("\t \t \t \t \t");
-		System.out.println("\t \t \t \t \t");
-		System.out.println("\t \t \t \t \t");
+		System.out.println("10\t 11\t 12\t 13\t 14\t");
+		System.out.println("15\t 16\t 17\t 18\t 19\t");
+		System.out.println("20\t 21\t 22\t 23\t 24\t");
 
 	}
 
---
+++ CS_10/src/Pizzasite/pizzasite.java
@@ -0,0 +1,11 @@
+package Pizzasite;
+
+public class pizzasite 
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
