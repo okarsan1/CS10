111b97827cccbbf3dd4a044f718e97d54164312a
17058003 <17058003@L815WD220005.edu.cbe.ab.ca>
2022-04-05 14:48:28 -0600

    rryyyryry

---
+++ CS_10/src/Pizzasite/PizzaCost.java
@@ -0,0 +1,24 @@
+package Pizzasite;
+
+import java.util.Scanner;
+
+public class PizzaCost {
+
+     public static void main(String[] args) {
+         
+         // Declare variables
+         Scanner sc = new Scanner(System.in);
+         double size = 0.0;
+         double cost = 0.0;
+         
+         // Display instructions
+         System.out.println("\nEnter the size of the pizza(diameter): ");
+         size = sc.nextDouble();
+         
+         // Calculate total cost
+         cost = ((.05 * size * size) + 1 + .75);
+         
+         // Output the total
+         System.out.println("Total cost: $" + cost);
+     }
+}
--- CS_10/src/Pizzasite/pizzasite.java
+++ CS_10/src/Pizzasite/pizzasite.java
@@ -1,14 +1,22 @@
 package Pizzasite;
-
-public class pizzasite
+import java.util.Scanner;
+public class pizzasite 
 {
-
-	public static void main(String[] args) 
-	{
-String txt = "1234567890";
-String ok = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-			    System.out.println("Enter the diameter of the pizza in inches:: " + txt.length());
-			    System.out.println("The cost of making the pizza is:: " + ok.length());
-	}
-
+public static void main(String[] args) 
+{
+		   // Declare variables
+        Scanner sc = new Scanner(System.in);
+        double size = 0.0;
+        double cost = 0.0;
+        
+        // Display instructions
+        System.out.println("\nEnter the size of the pizza(diameter): ");
+        size = sc.nextDouble();
+        
+        // Calculate total cost
+        cost = ((.05 * size * size) + 1 + .75);
+        
+        // Output the total
+        System.out.println("Total cost: $" + cost);
+    }
 }
