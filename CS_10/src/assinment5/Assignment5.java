1ce38874f4ee632df4993c590facf151e62111e8
17058003 <17058003@L815WD220005.edu.cbe.ab.ca>
2022-04-08 11:31:29 -0600

    gtrtrtrtr

---
+++ CS_10/src/assinment5/Assignment5.java
@@ -0,0 +1,46 @@
+package assinment5;
+/*
+Program: Assignment5.java         
+ Date: April 7, 2022
+Author: Omar Karsan 
+School: CHHS
+Course: Computer Science 10
+*/
+public class Assignment5 
+{
+public static void main(String[] args) 
+{
+	   // Declare variables
+     double cost = 0.0; 
+    // Display instructions
+     // Calculate total cost
+    cost = (79 + (3 * (4 + 82 - 68)) - 7 + 19);
+    // Output the total
+    System.out.println("\n79 + 3 * (4 + 82 –68) – 7 +19: " + cost);
+	  
+    // Declare variables
+    
+ double word = 0.0;
+ 
+ // Display instructions
+ 
+ 
+ // Calculate total cost
+ word = ((179 +21 +10) / 7 + 181 );
+ 
+ // Output the total
+ System.out.println("\n(179 +21 +10) / 7 + 181: " + word);
+ // Declare variables
+ double ok = 0.0;
+ // Display instructions
+ // Calculate total cost
+ ok = (10389 * 56 * 11 + 2246 );
+ // Output the total
+ System.out.println("\n10389 * 56 * 11 + 2246 : " + ok);	  
+}
+}
+/* Screen Dump
+79 + 3 * (4 + 82 –68) – 7 +19:
+(179 +21 +10) / 7 + 181:
+10389 * 56 * 11 + 2246 :
+*/
