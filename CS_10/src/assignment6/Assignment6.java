90cacc81a9510ce9d866a4177e807c05e7425d8f
17058003 <17058003@L815WD220005.edu.cbe.ab.ca>
2022-04-12 14:46:36 -0600

    tututuu

---
+++ CS_10/src/assignment6/Assignment6.java
@@ -0,0 +1,39 @@
+package assignment6;
+/*
+Program: Assignment5.java         
+ Date: April 7, 2022
+Author: Omar Karsan 
+School: CHHS
+Course: Computer Science 10
+*/
+import java.util.Scanner;
+
+public class Assignment6 {
+
+	 public static void main(String[] args) {
+		    try (Scanner myObj = new Scanner(System.in)) {
+		    	System.out.println("What is your first name?,What is your last name?,What grade are you in?,What school do you attend?,What is your favourite hobby?");
+
+				// String input
+				String name  = myObj.nextLine();;
+				String last  = myObj.nextLine();;
+				String grade  = myObj.nextLine();;
+				String school  = myObj.nextLine();;
+				String hobby  = myObj.nextLine();;
+				// Output input by user
+				System.out.println("Hello, your first  name is\n" + name);
+				System.out.println("And your last name is\n" + last);
+				System.out.println("you are currently in grade\n" + grade);
+				System.out.println("at\n" + school);
+				System.out.println("Your favourite hobby is\n" + hobby);
+	             
+				
+				
+			} 
+		  }
+		}
+/* Screen Dump
+
+What is your first name?,What is your last name?,What grade are you in?,What school do you attend?,What is your favourite hobby?:
+
+ */
