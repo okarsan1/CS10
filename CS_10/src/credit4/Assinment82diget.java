9cc492f15ea3fdbf59e0990ad0c6512747c91832
17058003 <17058003@L815WD220005.edu.cbe.ab.ca>
2022-04-22 10:55:29 -0600

    tytyyty

--- CS_10/src/credit4/Assinment82diget.java
+++ CS_10/src/credit4/Assinment82diget.java
@@ -8,51 +8,49 @@ package credit4;
 	School: CHHS
 	Course: Computer Science 10
 	*/
-	public class Assinment82diget {
-		
-			private static Scanner input;
+	public class Assinment82diget {private static Scanner input;
 
-			public static void main(String[] args) 
-			{
-				 input = new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+		 input = new Scanner(System.in);
 
-			    //Values of each digit
-			     int tens = 0;
-			    int ones = 0;
+	    //Values of each digit
+	     int first = 0;
+	    int second = 0;
 
-			    //Prompt user to input 2 digit number  
-			    System.out.println("Enter your 2 digit number:");
-			    int number = input.nextInt();
+	    //Prompt user to input 2 digit number  
+	    System.out.println("Enter an integer:");
+	    int number = input.nextInt();
 
-			if (number <= 99 && number > 10)   // Checking condition for two digit number
-			{
+	if (number <= 99 && number > 10)   // Checking condition for two digit number
+	{
 
-			    //Displays tens digit
-			    tens = (number - (tens)) / 10;  // compare with your code
-			    System.out.printf("\nTens place digit: " + tens);
+	    //Displays tens digit
+	    second= (number = (first*second));  // compare with your code
+	    System.out.printf("\nTens place digit: " + second);
 
 
-			    //Display ones digit
-			    ones = (number - (tens*10));   // compare with your code
-			    System.out.printf("\nOnes place digit: " + ones);   
-			}
-			                     //Error if number is less or more then two digits
-			else                                                                    
-			{      
-			if (number > 999)
-			    System.out.println("\nError! To many digets(more then 2).");
-			    if (number < 100)
-			    System.out.println("Error! To little digets (less then 2).");
-			}
+	    //Display ones digit
+	    first = (number = (first%second));   // compare with your code
+	    System.out.printf("\nOnes place digit: " + second);   
+	}
+	                     //Error if number is less or more then two digits
+	else                                                                    
+	{      
+	if (number > 999)
+	    System.out.println("\nError! To many digets(more then 2).");
+	    if (number < 100)
+	    System.out.println("Error! To little digets (less then 2).");
+	}
 
 
 
-			}
-			}
-	/* Screen Dump
-	 Enter your 2 digit number:
-	 Tens place digit:
-	 Ones place digit:
-	 Error! To many digets(more then 2).
-	 Error! To little digets (less then 2)
-	*/
+	}
+	}
+/* Screen Dump
+Enter your 2 digit number:
+Tens place digit:
+Ones place digit:
+Error! To many digets(more then 2).
+Error! To little digets (less then 2)
+*/
---
+++ CS_10/src/credit4/assinment9.java
@@ -0,0 +1,26 @@
+package credit4;
+
+import java.util.Scanner;
+public class assinment9 {
+
+	public static void main(String[] args) {
+		 Scanner userinput = new Scanner(System.in);  // Create a Scanner object
+		 System.out.println("Enter an integer:"); //user enters number
+		 int firstint = userinput.nextInt(); //grab and store number
+
+		 Scanner input = new Scanner(System.in);  // Create a Scanner object
+		 System.out.println("Enter a second interger:"); //user enters number
+		 int secondint = userinput.nextInt(); //grab and store number
+
+
+System.out.println(firstint + " / " + secondint + " = " + firstint / secondint +"\n"); // print out answer and calculate equation
+System.out.println(firstint + " % " + secondint + " = " + firstint % secondint + "\n"); // print out answer and calculate equation
+
+
+System.out.println(secondint + " / " + firstint + " = " + secondint / firstint + "\n"); // print out answer and calculate equation
+System.out.println(secondint + " % " + firstint + " = " + secondint % firstint + "\n"); // print out answer and calculate equation
+
+}
+	}
+
+
